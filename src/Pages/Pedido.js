a44effc5c344257645fcc98882fda6ef69426e2b
Eos Rebel Dusky <spando93@gmail.com>
2022-09-28 20:49:03 -0300

    Adding Generic Input

--- src/Pages/Pedido.js
+++ src/Pages/Pedido.js
@@ -2,7 +2,13 @@
 
 function Pedido(){
 
-return('')
+return(
+        <div> 
+            <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+            <TextField id="filled-basic" label="Filled" variant="filled" />
+            <TextField id="standard-basic" label="Standard" variant="standard" />
+        </div>
+)
 }
 
 export default Pedido;
