845c0b6f9385ba6b5b9d1d1bc01c43197def82b2
Eosrebeldusky <eosrebeldusky@gmail.com>
2022-10-22 19:52:36 -0300

    Agregada etiqueta

--- src/Pages/Account.js
+++ src/Pages/Account.js
@@ -1,18 +1,35 @@
 import React from "react";
+import { useState } from "react";
 import { TextField,Button } from "@mui/material";
 import {Link} from "react-router-dom";
+import { Label } from "@mui/icons-material";
 
 function Account (){
 
+    const [formValue,setFormValue] = useState({
+        email:""
+    })
+
+
+    const handleChange = (e) =>{
+        const {name,value} = e.target.value;
+        setFormValue((prevState) => {
+            return {...prevState,[name]:value};
+        }); 
+      }
+
     const handleSubmit = () =>{
-        alert('Enviado')
+        console.log(email)
     }
+
+    const email = formValue;
+    
     return (
         <>
         <h1>Cuenta</h1>
         <form onSubmit={handleSubmit}>
-        <TextField id="filled-basic" label="email" variant="filled" />
-        <TextField id="filled-basic" label="password" variant="filled" />
+        <label>E-mail</label>
+        <input type="email" name="email" value={email} onChange={handleChange}/>         
         <input type="submit"></input>
         </form>
         <Button><Link to="/Register" >Registrarse </Link></Button>
