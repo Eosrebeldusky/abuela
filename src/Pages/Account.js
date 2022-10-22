b72992ee6d067436f60295214b64e9d45b58fa37
Eosrebeldusky <eosrebeldusky@gmail.com>
2022-10-22 20:20:13 -0300

    Agregado formulario controlado

--- src/Pages/Account.js
+++ src/Pages/Account.js
@@ -4,32 +4,36 @@ import { TextField,Button } from "@mui/material";
 import {Link} from "react-router-dom";
 import { Label } from "@mui/icons-material";
 
+
 function Account (){
 
-    const [formValue,setFormValue] = useState({
-        email:""
+    const [form,setForm] = useState({ //armo estados para el formulario con los inputs que necesito
+        email:" ",
+        user:" "
     })
+    
 
+    const handleChange = (e) =>{ 
+    const value = e.target.value // en el manejador declaro una variable value y le pido que me traiga el valor de quien modifica.
+    setForm({...form, [e.target.name]:value})   //grabo por el nombre de quien modifico su valor
+    
+    }
+        
+    
 
-    const handleChange = (e) =>{
-        const {name,value} = e.target.value;
-        setFormValue((prevState) => {
-            return {...prevState,[name]:value};
-        }); 
-      }
-
-    const handleSubmit = () =>{
-        console.log(email)
+    const handleSubmit = () => {
+        console.log(form)
     }
 
-    const email = formValue;
-    
+       
     return (
         <>
         <h1>Cuenta</h1>
         <form onSubmit={handleSubmit}>
         <label>E-mail</label>
-        <input type="email" name="email" value={email} onChange={handleChange}/>         
+        <input type="email" name="email" value={form.email} onChange={handleChange}/>   
+        <label>User</label>      
+        <input type="text" name="user" value={form.user} onChange={handleChange}/>      
         <input type="submit"></input>
         </form>
         <Button><Link to="/Register" >Registrarse </Link></Button>
