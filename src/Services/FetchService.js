8280c3843d4537ce8dcfbadb333581a4980fbf0c
Eosrebeldusky <eosrebeldusky@gmail.com>
2022-10-25 17:32:20 -0300

    service

--- src/Services/FetchService.js
+++ src/Services/FetchService.js
@@ -1,12 +1,12 @@
 import {db,app,firestore} from '../Services/Config'
-import { collection, getDocs, } from "firebase/firestore";
+import { collection, getDocs, data} from "firebase/firestore";
 
 
 export const getAllConsole = () =>{
     async function readDocument(){      
         const querySnapshot = await getDocs(collection(db, 'velas'));
           querySnapshot.forEach((doc) => {  
-          console.log(`read document, ${doc.id} => ${doc.titulo}`);
+          console.log(`read document, ${doc.id} => ${doc.data("titulo")}`);
          });
         }
         readDocument()
