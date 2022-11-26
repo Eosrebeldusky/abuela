98146fdedb5e82820f0ee2178fb616bef916361b
Eosrebeldusky <eosrebeldusky@gmail.com>
2022-11-26 17:23:57 -0300

    Add presented function

--- src/Pages/Crud.js
+++ src/Pages/Crud.js
@@ -3,12 +3,13 @@ import AddItem from '../Components/AddItem'
 
 function Crud(){
 
-
+const add =() =>{
+    console.log('new task')
+}
 
 
     return(
-        <>
-        
+        <>        
         <AddItem/>
         
         </>
