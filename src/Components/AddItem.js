08aea42f2230591fbcd525f63cdaea9ede94f4db
Eosrebeldusky <eosrebeldusky@gmail.com>
2022-11-26 17:13:29 -0300

    Finished handleChange and sorta handle submit

--- src/Components/AddItem.js
+++ src/Components/AddItem.js
@@ -16,29 +16,31 @@ function AddItem(){
         console.log(values)
     }  
 
-    const handleChange = e =>{
-         
+    const handleChange = e =>{ 
+        const {name,value} = e.target;
+        console.log(name, value)
+        setValues({...values,[name]:value} )
     }
-    
+     
     return( 
         <>
         <h1>Agregar item</h1>
             <form id= 'AddCandle' onSubmit={handleSubmit}>
                 <label>
                     Titulo:
-                    <input type="text" name="titulo"/>
+                    <input type="text" name="titulo" onChange={handleChange}/>
                 </label>
                 <label>
                     Descripcion:
-                    <input type="text" name="descripcion"/>
+                    <input type="text" name="descripcion" onChange={handleChange}/>
                 </label>
                 <label>
                     Precio:
-                    <input type="text" name="precio"/>
+                    <input type="text" name="precio" onChange={handleChange}/>
                 </label>
                 <label>
                     Categoria:
-                    <input type="text" name="categoria"/>
+                    <input type="text" name="categoria" onChange={handleChange}/>
                 </label>                
                 <input type="submit" value="Submit"/>
             </form>    
